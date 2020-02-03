e3ce6ae88ea2f8ca7bfe133b48dda8672325772c
Nyi Nyi Aung <anyinyi191@gmail.com>
2020-02-03 17:02:47 +0800

    commit

--- app/src/main/java/com/example/psbquiz/Activities/Common.java
+++ app/src/main/java/com/example/psbquiz/Activities/Common.java
@@ -1,17 +1,4 @@
 package com.example.psbquiz.Activities;
 
 public class Common {
-    public int score;
-
-    public Common(int score){
-        this.score=score;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score){
-        this.score = score;
-    }
 }
